ea93a93291574d3cb29ce782deb0e3b6d1ebefc7
Ayush Ghavghave <ghavghaveayush782@gmail.com>
2024-11-25 21:46:31 +0530

    Fix submodule or directory issue

--- 01_vitereact/src/App.jsx
+++ 01_vitereact/src/App.jsx
@@ -1,10 +1,10 @@
-
+import Chai from "./Chai"
 
 function App() {
 
   return (
     <>
-      <h1>React with vite | Ayush Ghavghave</h1>
+      <Chai/>
     </>
   )
 }
---
+++ 01_vitereact/src/Chai.jsx
@@ -0,0 +1,10 @@
+function Chai(){
+    return(
+        <>
+        <h1>Chai is Ready</h1>
+        <p>chai is cold</p>
+        </>
+    )
+}
+
+export default Chai
