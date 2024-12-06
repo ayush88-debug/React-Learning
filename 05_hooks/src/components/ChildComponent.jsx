3751c988a7506c0636f05bc36c8cd3af6833e3b9
Ayush Ghavghave <ghavghaveayush782@gmail.com>
2024-12-06 15:35:09 +0530

    commit

--- 05_hooks/src/components/ChildComponent.jsx
+++ 05_hooks/src/components/ChildComponent.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 export default React.memo(function ChildComponent({name, alertFunction}) {  ////React.memo -> wrap -> component -> component re-render tabhi hoga jab props change honge nahi toh re-render nahi hoga 
 
-                                                                            //if u r sending a function, then react.memo wont be able to save you from re-rendering
+//if u r sending a function, then react.memo wont be able to save you from re-rendering
     const msg="Child componet is re-rendered"
     console.log(msg)
     alertFunction()
